ef7e44dea3ff1ee6041f362b8054bbf4bb1eb7d0
b21antbr <b21antbr@student.his.se>
2024-03-28 16:41:11 +0100

    added icons to posts, changed horizontal to fit plan

--- app/src/main/java/com/example/highDemand_xml/MainActivity.kt
+++ app/src/main/java/com/example/highDemand_xml/MainActivity.kt
@@ -5,6 +5,7 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
@@ -62,45 +63,24 @@ class MainActivity : AppCompatActivity() {
 
         // Loop to add multiple instances of RelativeLayout
         val numberOfItems = 10
+        val marginInDp = 16
+        val density = resources.displayMetrics.density
+
         for (i in 0 until numberOfItems) {
-            // Create a new RelativeLayout
-            val relativeLayout = RelativeLayout(this)
-            relativeLayout.layoutParams = layoutParams
+            // Inflate layout based on even or odd
+            val layoutResId = if (i % 2 == 0) R.layout.imageitem_layout else R.layout.imageitem2_layout
+            val constraintLayout = LayoutInflater.from(this).inflate(layoutResId, null) as ConstraintLayout
 
+            // Add the ConstraintLayout to the LinearLayout
+            linearLayout.addView(constraintLayout)
 
-            // Create a new ImageView
-            val imageView = ImageView(this)
-            imageView.id = View.generateViewId()
-            imageView.layoutParams = RelativeLayout.LayoutParams(
-                100, // Width
-                100 // Height
-            )
-            imageView.scaleType = ImageView.ScaleType.CENTER_CROP
-            if (i % 2 == 0) {
-                imageView.setImageResource(R.drawable.photo3) // Set your image resource here
-            } else {
-                imageView.setImageResource(R.drawable.photo4) // Set your image resource here
+            // Add space between items
+            if (i < numberOfItems - 1) {
+                val spaceView = View(this)
+                val params = LinearLayout.LayoutParams((marginInDp * density).toInt(), LinearLayout.LayoutParams.MATCH_PARENT)
+                spaceView.layoutParams = params
+                linearLayout.addView(spaceView)
             }
-            relativeLayout.addView(imageView)
-
-            // Create a new TextView
-            val textView = TextView(this)
-            textView.layoutParams = RelativeLayout.LayoutParams(
-                RelativeLayout.LayoutParams.WRAP_CONTENT,
-                RelativeLayout.LayoutParams.WRAP_CONTENT
-            )
-            textView.text = "Text"
-            textView.setTextColor(Color.BLACK)
-            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 12f)
-            val textParams = textView.layoutParams as RelativeLayout.LayoutParams
-            textParams.addRule(RelativeLayout.BELOW, imageView.id)
-            textParams.addRule(RelativeLayout.CENTER_HORIZONTAL)
-            textView.layoutParams = textParams
-            relativeLayout.addView(textView)
-
-            // Add the RelativeLayout to the LinearLayout
-            linearLayout.addView(relativeLayout)
-
         }
 
         val scrollView = findViewById<ScrollView>(R.id.scrollView)
@@ -110,22 +90,17 @@ class MainActivity : AppCompatActivity() {
 
         for (i in 0 until numberOfPosts) {
             // Inflate the card layout
-            if(i%2 == 0){
-                val cardLayout = layoutInflater.inflate(R.layout.post1_layout, null) as ConstraintLayout
-                val imageView = cardLayout.findViewById<ImageView>(R.id.imageView)
-                imageView.setImageResource(R.drawable.photo3) // Set your image resource here
-                // Add the card layout to the linear layout inside the ScrollView
-                linearLayout2.addView(cardLayout)
-            }
-            else{
-                val cardLayout = layoutInflater.inflate(R.layout.post2_layout, null) as ConstraintLayout
-                val imageView = cardLayout.findViewById<ImageView>(R.id.imageView)
-                imageView.setImageResource(R.drawable.photo4) // Set your image resource here
-                // Add the card layout to the linear layout inside the ScrollView
-                linearLayout2.addView(cardLayout)
-            }
-
-
+            val cardLayout = layoutInflater.inflate(if (i % 2 == 0) R.layout.post1_layout else R.layout.post2_layout, null) as ConstraintLayout
+            val imageView = cardLayout.findViewById<ImageView>(R.id.imageView)
+            val imageViewIcon1 = cardLayout.findViewById<ImageView>(R.id.heartIcon)
+            val imageViewIcon2 = cardLayout.findViewById<ImageView>(R.id.shareIcon)
+            val imageViewIcon3 = cardLayout.findViewById<ImageView>(R.id.commentIcon)
+            imageView.setImageResource(if (i % 2 == 0) R.drawable.photo3 else R.drawable.photo4) // Set your image resource here
+            imageViewIcon1.setImageResource (R.drawable.heart_121) // Set your image resource here
+            imageViewIcon2.setImageResource (R.drawable.share) // Set your image resource here
+            imageViewIcon3.setImageResource (R.drawable.vector) // Set your image resource here
+            // Add the card layout to the linear layout inside the ScrollView
+            linearLayout2.addView(cardLayout)
         }
 
         val post1Images = MutableList(9) { R.drawable.photo3 }
